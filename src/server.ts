d8095ef1a672acec80496c7ecc45623179bbca8c
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-04-21 13:46:08 -0300

    feat: Configurando o ESlint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { env } from "./env";
-import { app } from "./app";
+import { env } from './env'
+import { app } from './app'
 
-app.listen({
-    host: '0.0.0.0', //Evitar problmas ao connectar com o front end
+app
+  .listen({
+    host: '0.0.0.0', // Evitar problmas ao connectar com o front end
     port: env.PORT,
-}).then(() => {
-    console.log('HTTP Server Running !!')
-})
+  })
+  .then(() => {
+    console.log(`HTTP Server Running!! Porta: ${env.PORT}`)
+  })
