9cfbb22f749cfa9849c75036a9df7bb073f110ef
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-04-22 11:31:19 -0300

    feat: Criando o docker e mais algumas tabelas no prisma

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,22 +1,22 @@
-import 'dotenv/config';
-import { z } from 'zod';
+import 'dotenv/config'
+import { z } from 'zod'
 
-//Validando as variaveis de abiente
+// Validando as variaveis de abiente
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'), // Essa variavel só pode ter esses tipos
-    PORT: z.coerce.number().default(3333), //Converte um valor para um numero
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'), // Essa variavel só pode ter esses tipos
+  PORT: z.coerce.number().default(3333), // Converte um valor para um numero
 })
 
-//SafeParse vai tentar validar o process.env
+// SafeParse vai tentar validar o process.env
 const _env = envSchema.safeParse(process.env)
 
 // O _env.error.format() - Pega tudo que deu o erro e formata para um jeito mais simples de ver
-if(_env.success === false){
-    console.error('Invalid environment variables', _env.error.format())
+if (_env.success === false) {
+  console.error('Invalid environment variables', _env.error.format())
 
-    // O throw new Error vai quebrar a aplicação se ocorrer um erro no env
-    throw new Error('Invalid environment variables')
+  // O throw new Error vai quebrar a aplicação se ocorrer um erro no env
+  throw new Error('Invalid environment variables')
 }
 
 // retorna o valor da variaveis
