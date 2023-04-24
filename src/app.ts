1b473ca3ab5cb5b6a29dc91a93ed99c9b0963b5d
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-04-24 10:30:38 -0300

    feat: Criando a rota post de users e refatorando o prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodyShema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodyShema.parse(request.body) // Parse fazer igual um throw Error se falhar, para por aqui
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
