8c9da6e975dfd5f6b96e9110ab9d9a00323328bc
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-05-04 18:01:21 -0300

    feat: Primeiro teste unitario

--- src/useCases/register.spec.ts
+++ src/useCases/register.spec.ts
@@ -1,5 +1,36 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('Check', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('Should hash user password upon registration', async () => {
+    const registerUserCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUserCase.execute({
+      name: 'Italo Araujo',
+      email: 'italoarauju258@gmail.com',
+      password: '123456789',
+    })
+
+    const isPasswordCorrectLyHashed = await compare(
+      '123456789',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectLyHashed).toBe(true)
+  })
 })
