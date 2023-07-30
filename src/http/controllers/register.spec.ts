d3ce03b43e9ca404487bcf77369e1e42a7c226bc
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-30 15:11:41 -0300

    feat: Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,2 +1,23 @@
-import { test } from "vitest";
-test("ok", () => {});
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "Italo",
+      email: "italo@gmail.com",
+      password: "password",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
