c06d37a6ee7f51f651c2cebb9b7dbb5bcb790800
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-12 11:31:00 -0300

    feat: Implementação do refresh token

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,34 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify({ onlyCookie: true }); // Vai olhar para os cookies da nossa req, pra ver se existe
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    }
+  );
+
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: "7d",
+      },
+    }
+  );
+
+  return reply
+    .setCookie("refreshToken", refreshToken, {
+      path: "/", // Todo o back end pode ler esse valor do cokkie
+      secure: true, // Vai ser criptografado atraves do HTTPS, independete se ta usando HTTPs ou não
+      sameSite: true, // Esse cookie so vai ser acessado no mesmo dominimo do site
+      httpOnly: true, // Vai ser acessado só pelo backEnd e não pelo front end
+    })
+    .status(200)
+    .send({ token });
+}
